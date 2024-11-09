3d0ab600d4dec3f189970672b066c60e01bc3a9b
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-11-09 17:32:08 +1100

    uncomment code

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,21 +2,20 @@ import {db} from "../../../../server/db"
 
 export const POST = async (req: Request) => {
   const {data} = await req.json()
-  console.log(data)
-  // const emailAddress = data.email_addresses[0].email_address
-  // const lastName = data.last_name
-  // const firstName = data.first_name
-  // const imageUrl = data.image_url
-  // const id = data.id
+  const emailAddress = data.email_addresses[0].email_address
+  const lastName = data.last_name
+  const firstName = data.first_name
+  const imageUrl = data.image_url
+  const id = data.id
   
-  // await db.user.create({
-  //   data: {
-  //     id: id,
-  //     emailAddress: emailAddress,
-  //     firstName: firstName,
-  //     lastName: lastName,
-  //   }
-  // })
+  await db.user.create({
+    data: {
+      id: id,
+      emailAddress: emailAddress,
+      firstName: firstName,
+      lastName: lastName,
+    }
+  })
 
   return new Response('Webhook received', {status: 200})
 }
