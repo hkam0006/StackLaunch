4106c322eedf6b731b8d8f3ed30f3df7982a63b3
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-11-22 14:12:00 +1100

    fixed bug

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export function buildProject(id: string){
   return new Promise((resolve) => {
-    const child = exec(`cd ${path.join(process.env.WORK_DIR as string, `output/${id}`)} && npm install && npm run build`)
+    const child = exec(`cd ${path.join(process.env.WORK_DIR as string, id)} && npm install && npm run build`)
 
     child.stdout?.on('data', (data) => {
       console.log('stdout: ', data)
